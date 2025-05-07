41ec5e3c8b08aa4b6b9303f89aefb53199e2c38d
Haruki Shimizu <shumizu418128@gmail.com>
2025-05-07 09:49:23 +0900

    html返すだけ

--- main.go
+++ main.go
@@ -9,8 +9,13 @@ func route(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, World!")
 }
 
+func test_page(w http.ResponseWriter, r *http.Request) {
+    http.ServeFile(w, r, "test.html")
+}
+
 func main() {
     http.HandleFunc("/", route)
+    http.HandleFunc("/test", test_page)
     http.ListenAndServe(":10000", nil)
 	fmt.Println("Server started at http://localhost:10000")
 }
