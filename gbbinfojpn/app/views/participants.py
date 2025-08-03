3e44b8f639bf0524a0452be3cb8b27c8b97062c4
Haruki Shimizu <shumizu418128@gmail.com>
2025-08-03 13:34:18 +0900

    いらない情報とってたので削除

--- gbbinfojpn/app/views/participants.py
+++ gbbinfojpn/app/views/participants.py
@@ -112,10 +112,10 @@ def participants_view(request: HttpRequest, year: int):
     # 出場者データを取得
     participants_data = supabase_service.get_data(
         table="Participant",
-        columns=["name", "category", "ticket_class", "is_cancelled", "iso_code"],
+        columns=["id", "name", "category", "ticket_class", "is_cancelled", "iso_code"],
         join_tables={
             "Category": ["id", "name"],
-            "ParticipantMember": ["participant", "name"],
+            "ParticipantMember": ["name"],
             "Country": ["iso_code", "names"],
         },
         filters=filters,
@@ -176,7 +176,7 @@ def participants_country_specific_view(request: HttpRequest, year: int):
         columns=["name", "category", "ticket_class", "is_cancelled", "iso_code"],
         join_tables={
             "Category": ["id", "name"],
-            "ParticipantMember": ["participant", "name"],
+            "ParticipantMember": ["name"],
         },
         filters={
             "year": year,
