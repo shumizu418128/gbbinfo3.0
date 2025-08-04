e639e3eb03518e301f6dd882752658a977792335
Haruki Shimizu <shumizu418128@gmail.com>
2025-08-04 21:13:04 +0900

    langの仕様を見直し

--- gbbinfojpn/app/views/language.py
+++ gbbinfojpn/app/views/language.py
@@ -1,3 +1,5 @@
+from urllib.parse import urlparse
+
 from django.conf import settings
 from django.http import HttpRequest, HttpResponseRedirect
 
@@ -12,15 +14,24 @@ def change_language(request: HttpRequest):
     Returns:
         HttpResponseRedirect: リダイレクトレスポンス
     """
-    lang_code = request.GET.get("lang")
-    referrer = request.GET.get("referrer", "/")
+    lang_code = request.GET.get("to")
 
     # サポートされている言語か確認
     if lang_code not in settings.SUPPORTED_LANGUAGE_CODES:
         lang_code = "ja"
 
+    # 直前のページ（リファラー）を取得する
+    current_url = request.META.get("HTTP_REFERER", "/")
+    parsed_url = urlparse(current_url)
+
+    # 内部URLかつパスが/で始まる場合のみ許可
+    if parsed_url.netloc and parsed_url.netloc != request.get_host():
+        current_url = "/"
+    elif not parsed_url.path.startswith("/"):
+        current_url = "/"
+
     # クッキーにも保存し、referrerにリダイレクト
-    response = HttpResponseRedirect(referrer)
+    response = HttpResponseRedirect(current_url)
     response.set_cookie(settings.LANGUAGE_COOKIE_NAME, lang_code)
 
     return response
