6322569914ff0d62689b88e629cfc88460eed88c
Haruki Shimizu <shumizu418128@gmail.com>
2025-08-06 11:30:44 +0900

    静的ファイルを提供するビューを追加し、関連するURLパターンを設定

--- gbbinfojpn/urls.py
+++ gbbinfojpn/urls.py
@@ -4,6 +4,15 @@ from django.http import HttpResponse
 from django.urls import include, path
 
 from gbbinfojpn.app.sitemaps import StaticViewSitemap
+from gbbinfojpn.app.views.static_files import (
+    ads_txt,
+    apple_touch_icon,
+    discord,
+    favicon_ico,
+    manifest_json,
+    robots_txt,
+    service_worker_js,
+)
 
 sitemaps = {
     "static": StaticViewSitemap,
@@ -11,14 +20,39 @@ sitemaps = {
 
 urlpatterns = [
     # システム監視
-    path("health/", lambda _: HttpResponse("OK"), name="health_check"),
-    # サイトマップ
+    path("health", lambda _: HttpResponse("OK"), name="health_check"),
+    # 静的ファイル
     path(
         "sitemap.xml",
         sitemap,
         {"sitemaps": sitemaps},
         name="django.contrib.sitemaps.views.sitemap",
     ),
+    path("ads.txt", ads_txt, name="ads_txt"),
+    path("manifest.json", manifest_json, name="manifest_json"),
+    path("robots.txt", robots_txt, name="robots_txt"),
+    path("favicon.ico", favicon_ico, name="favicon_ico"),
+    path("service-worker.js", service_worker_js, name="service_worker_js"),
+    path(
+        "apple-touch-icon-152x152-precomposed.png",
+        apple_touch_icon,
+        name="apple_touch_icon_152_precomposed",
+    ),
+    path("apple-touch-icon-152x152.png", apple_touch_icon, name="apple_touch_icon_152"),
+    path(
+        "apple-touch-icon-120x120-precomposed.png",
+        apple_touch_icon,
+        name="apple_touch_icon_120_precomposed",
+    ),
+    path("apple-touch-icon-120x120.png", apple_touch_icon, name="apple_touch_icon_120"),
+    path(
+        "apple-touch-icon-precomposed.png",
+        apple_touch_icon,
+        name="apple_touch_icon_precomposed",
+    ),
+    path("apple-touch-icon.png", apple_touch_icon, name="apple_touch_icon"),
+    path(".well-known/discord", discord, name="discord"),
+    # アプリケーションのURL
     path("", include("gbbinfojpn.app.urls", namespace="app")),
 ]
 
