1f23b97c96bddbe08f1690fbc803652026009db7
Haruki Shimizu <shumizu418128@gmail.com>
2025-11-08 16:14:30 +0900

    htmlでの定義に変更

--- app/static/scripts/ui.js
+++ app/static/scripts/ui.js
@@ -62,15 +62,15 @@ async function loadNotice() {
         if (data.notice && data.notice.trim() !== '' && data.timestamp) {
             const noticeDivs = document.querySelectorAll('.notice');
             noticeDivs.forEach((noticeDiv) => {
-                const postIt = document.createElement('div');
-                postIt.className = 'post-it-notice';
+                const postItNotice = noticeDiv.querySelector('.post-it-notice');
+                const noticeContent = noticeDiv.querySelector('.notice-content');
+                const noticeTimestamp = noticeDiv.querySelector('.notice-timestamp');
 
-                const noticeIcon = '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#222222"><path d="M440-440h80v-200h-80v200Zm40 120q17 0 28.5-11.5T520-360q0-17-11.5-28.5T480-400q-17 0-28.5 11.5T440-360q0 17 11.5 28.5T480-320ZM160-200v-80h80v-280q0-83 50-147.5T420-792v-28q0-25 17.5-42.5T480-880q25 0 42.5 17.5T540-820v28q80 20 130 84.5T720-560v280h80v80H160Zm320-300Zm0 420q-33 0-56.5-23.5T400-160h160q0 33-23.5 56.5T480-80ZM320-280h320v-280q0-66-47-113t-113-47q-66 0-113 47t-47 113v280Z"/></svg>'
-                const paragraph = document.createElement('p');
-                paragraph.innerHTML = noticeIcon + '<br><strong>管理人からのお知らせ</strong><br>' + data.notice + "<br><br>最終更新<br>" + data.timestamp;
-
-                postIt.appendChild(paragraph);
-                noticeDiv.appendChild(postIt);
+                if (postItNotice && noticeContent && noticeTimestamp) {
+                    noticeContent.textContent = data.notice;
+                    noticeTimestamp.textContent = data.timestamp;
+                    postItNotice.style.display = 'block';
+                }
             });
         }
     } catch (error) {
