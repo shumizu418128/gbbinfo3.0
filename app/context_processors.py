46bf3ff10d752db05d72fbb8a48a0ff1784c1d49
Haruki Shimizu <shumizu418128@gmail.com>
2025-12-17 19:19:13 +0900

    年度のバグ修正

--- app/context_processors.py
+++ app/context_processors.py
@@ -311,7 +311,7 @@ def common_variables(
             - is_pull_request (bool): プルリクエスト環境かどうか
             - scroll (str): スクロール位置（クエリパラメータ）
     """
-    year_str = request.path.split("/")[1]
+    year_str = request.path.split("/")[2]
 
     # 年度が最新 or 試験公開年度か検証
     try:
